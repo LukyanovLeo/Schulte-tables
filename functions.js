03abca4e0a4fa466a3fdf649817efbf05c22f257
SvetikSchulman <lev06191972@yandex.ru>
2017-02-18 09:12:21 +0300

    18.02

--- functions.js
+++ functions.js
@@ -4,13 +4,6 @@ Raphael.el.green = function () {
 		stroke: "green"
 	});
 };
-Raphael.el.red = function () {
-    this.attr({
-		fill: "white",
-		stroke: "red"
-	});
-};
-
 function toStringPath(a, b, c, d){
 	var str = "M" + a + "," + b + "L" + c + "," + d;
 	return str;
@@ -44,45 +37,7 @@ function isFullBin(arr){
 	}
 	return true;
 }
-var paper = Raphael(100, 200, 1000, 1000);
-function toDrawNodes(){
-	paper.clear();
-	paper.setStart();
-	for (var i = 0; i < numberOfPaths; i++){
-			paper.path(ways[i]).green();
-		}
-		for (var i = 0; i < numberOfNodes; i++){
-			paper.circle(COORD_X[i], COORD_Y[i], CIRCLE_RADIUS).green();
-			paper.text(COORD_X[i], COORD_Y[i], VALUE[i]).green();
-		}	
-		for (var i = 0; i < numberOfPaths; i++){
-			switch(i){
-				case 0: paper.text(COORD_EDGE_X[i], COORD_EDGE_Y[i], WEIGHT_OF_EDGE[i]);
-						paper.text(COORD_X[i], COORD_Y[i]-25, marks[i]).red();break;
-				case 1: paper.text(COORD_EDGE_X[i], COORD_EDGE_Y[i], WEIGHT_OF_EDGE[i]); 
-						paper.text(COORD_X[i]-20, COORD_Y[i]-20, marks[i]).red();break;
-				case 2: paper.text(COORD_EDGE_X[i], COORD_EDGE_Y[i], WEIGHT_OF_EDGE[i]);
-						paper.text(COORD_X[i], COORD_Y[i]-25, marks[i]).red();break;
-				case 3: paper.text(COORD_EDGE_X[i], COORD_EDGE_Y[i], WEIGHT_OF_EDGE[i]); 
-						paper.text(COORD_X[i]+25, COORD_Y[i], marks[i]).red();break;
-				case 4: paper.text(COORD_EDGE_X[i], COORD_EDGE_Y[i], WEIGHT_OF_EDGE[i]); 
-						paper.text(COORD_X[i], COORD_Y[i]+25, marks[i]).red();break;
-				case 5: paper.text(COORD_EDGE_X[i], COORD_EDGE_Y[i], WEIGHT_OF_EDGE[i]); 
-						paper.text(COORD_X[i]+20, COORD_Y[i]+20, marks[i]).red();break;
-				case 6: paper.text(COORD_EDGE_X[i], COORD_EDGE_Y[i], WEIGHT_OF_EDGE[i]);
-						paper.text(COORD_X[i], COORD_Y[i]-25, marks[i]).red();break;
-				case 7: paper.text(COORD_EDGE_X[i], COORD_EDGE_Y[i], WEIGHT_OF_EDGE[i]); 
-						paper.text(COORD_X[i]+25, COORD_Y[i], marks[i]).red();break;
-				case 8: paper.text(COORD_EDGE_X[i], COORD_EDGE_Y[i], WEIGHT_OF_EDGE[i]); break;
-						
-				case 9: paper.text(COORD_EDGE_X[i], COORD_EDGE_Y[i], WEIGHT_OF_EDGE[i]); break;
-						
-				case 10: paper.text(COORD_EDGE_X[i], COORD_EDGE_Y[i], WEIGHT_OF_EDGE[i]); break;
-						
-			}
-		}
-	var st = paper.setFinish();
-}
+
 
 
 
--- matrix.js
+++ matrix.js
@@ -3,88 +3,16 @@ var matrix = [];
 for (var i = 0; i < numberOfNodes; i++){
 	matrix[i] = [];
 	for (var j = 0; j < numberOfNodes; j++){
-		for (var k = 0; k < numberOfPaths; k++){
+		for (var k = 0; k < numberOfEdges; k++){
 			if (toStringPath(COORD_X[i], COORD_Y[i], COORD_X[j], COORD_Y[j]) == ways[k]){
-				if ((i == 0) && (j == 1))
-					matrix[i][j] = 70;
-				//break;
-				if ((i == 0) && (j == 2))
-					matrix[i][j] = 30;
-				//break;
-				if ((i == 0) && (j == 4))
-					matrix[i][j] = 10;
-				//break;
-				if ((i == 0) && (j == 6))
-					matrix[i][j] = 20;
-				//break;
-				if ((i == 1) && (j == 3))
-					matrix[i][j] = 30;
-				//break;
-				if ((i == 1) && (j == 4))
-					matrix[i][j] = 20;
-				//break;
-				if ((i == 1) && (j == 5))
-					matrix[i][j] = 20;
-				//break;
-				if ((i == 2) && (j == 5))
-					matrix[i][j] = 30;
-				//break;
-				if ((i == 6) && (j == 3))
-					matrix[i][j] = 40;
-				//break;
-				if ((i == 6) && (j == 5))
-					matrix[i][j] = 20;
-				//break;
-				if ((i == 6) && (j == 7))
-					matrix[i][j] = 20;
-				//break;	
-				if ((i == 4) && (j == 6))
-					matrix[i][j] = 10;
-				break;
-				
-				
+				matrix[i][j] = 1;	break;
 			}
 			else if (toStringPath(COORD_X[j], COORD_Y[j], COORD_X[i], COORD_Y[i]) == ways[k]){
-				if ((i == 1) && (j == 0))
-					matrix[i][j] = 70;
-				//break;
-				if ((i == 2) && (j == 0))
-					matrix[i][j] = 30;
-				//break;
-				if ((i == 4) && (j == 0))
-					matrix[i][j] = 10;
-				//break;
-				if ((i == 6) && (j == 0))
-					matrix[i][j] = 20;
-				//break;
-				if ((i == 3) && (j == 1))
-					matrix[i][j] = 30;
-				//break;
-				if ((i == 4) && (j == 1))
-					matrix[i][j] = 20;
-				//break;
-				if ((i == 5) && (j == 2))
-					matrix[i][j] = 30;
-				//break;
-				if ((i == 3) && (j == 6))
-					matrix[i][j] = 40;
-				//break;
-				if ((i == 5) && (j == 6))
-					matrix[i][j] = 20;
-				//break;
-				if ((i == 7) && (j == 6))
-					matrix[i][j] = 20;
-				//break;
-				if ((i == 6) && (j == 4))
-					matrix[i][j] = 10;
-				//break;
-				if ((i == 5) && (j == 1))
-					matrix[i][j] = 20;
-				break;
+				matrix[i][j] = 1;	break;
 			}				
 			else {
 				matrix[i][j] = 0;
 			}			
 		}
-	}
+	}	
 }
